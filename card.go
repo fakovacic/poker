9ab9620c2a4c345db6cd5f1147ca091aed073242
fakovacic <filip.ante.kovacic@gmail.com>
2022-01-30 13:38:49 +0100

    card stringer interface

--- card.go
+++ card.go
@@ -1,6 +1,12 @@
 package poker
 
+import "fmt"
+
 type Card struct {
 	Suite Suite `json:"suite"`
 	Rank  Rank  `json:"rank"`
+}
+
+func (c Card) String() string {
+	return fmt.Sprintf("%s%s", c.Rank, c.Suite)
 }
